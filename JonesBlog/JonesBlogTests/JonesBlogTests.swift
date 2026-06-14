3b2b68f60b7f82ccb3a953483d0bd2c5637c45c7
Rog <rog@hatbat.net>
2026-06-14 10:07:10 +0100

    Add JonesBlog iOS project

---
+++ JonesBlog/JonesBlogTests/JonesBlogTests.swift
@@ -0,0 +1,19 @@
+//
+//  JonesBlogTests.swift
+//  JonesBlogTests
+//
+//  Created by Roger Nolan on 14/06/2026.
+//
+
+import Testing
+@testable import JonesBlog
+
+struct JonesBlogTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
